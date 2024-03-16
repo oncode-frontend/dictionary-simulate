b74c277f17abd164ad08a547b60f1d97eab1a4c8
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-16 18:13:02 +0330

    get elem from dom

---
+++ script.js
@@ -0,0 +1,9 @@
+const $ = document
+const getResultElem = $.querySelector('#result')
+const getBtnElem = $.querySelector('#search-btn')
+const getInputElem = $.querySelector('#inp-word')
+const getAudioElem = $.querySelector('#sound')
+
+
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
